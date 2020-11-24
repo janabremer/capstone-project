c01160e152e1906b2e771ab4666381b06d091565
Jana Bremer <jana.bremer@posteo.de>
2020-11-24 15:45:36 +0100

    Setup backend and test product API

---
+++ backend/src/Controller/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
+use App\Entity\Product;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/products", methods={"GET"})
+     */
+    public function index(
+        ProductRepository $repository, 
+        SerializerInterface $serializer): JsonResponse
+    {
+        $products = $repository->findAll();
+
+        return new JsonResponse(
+            $serializer->serialize($products, 'json'),
+            JsonResponse::HTTP_OK,
+            [],
+            true
+        );
+
+    }
+
+    /**
+     * @Route("/products/{id}", methods={"GET"})
+     */
+    public function findId(
+        int $id, 
+        ProductRepository $repository, 
+        SerializerInterface $serializer): JsonResponse 
+    {
+        $product = $repository->find($id);
+        if (is_null($product)) {
+            return new JsonResponse(['success' => false], JsonResponse::HTTP_NOT_FOUND);    
+        }
+        return new JsonResponse(
+            $serializer->serialize($product, 'json'), 
+            JsonResponse::HTTP_OK, 
+            [], 
+            true
+        );
+    }
+
+
+}
