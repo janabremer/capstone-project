00dd5a9b70b39e8cdb41e6da7a66817462a9327e
Jana Bremer <jana.bremer@posteo.de>
2020-11-24 17:25:06 +0100

    Fetch products API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    getProductsData().then(data => setProducts(data))
+  }, [])
+  function getProductsData() {
+    return fetch('http://virtualwater.local/products')
+      .then(res => res.json())
+  }
+
   return (
     <div className="App">
-      Hello World
+      <ul>
+        {products.map(({id, name, virtualWater}) => <li key={id} >{name}: {virtualWater} litres/kg</li>)
+        }
+      </ul>
     </div>
   );
 }
+
+
+
 
 export default App;
