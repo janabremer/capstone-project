812143d82b6ecda529834e3015fd2a8b45c6040f
Jana Bremer <jana.bremer@posteo.de>
2020-11-27 14:39:15 +0100

    Add tests for getPhoto and getProducts

---
+++ frontend/src/services/getPhoto.test.js
@@ -0,0 +1,29 @@
+import getPhoto from './getPhoto'
+
+describe('getPhoto', () => {
+    it('returns a photo object for the queryParam', async () => {
+  
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () =>
+            Promise.resolve({
+                "page": 1,
+                "per_page": 1,
+                "photos": [],
+                "total_results": 1000,
+                "next_page": "https://api.pexels.com/v1/search/?page=2&per_page=1&query=tomatoes"
+            }),
+        })
+      )
+      const result = await getPhoto('tomatoes')
+      expect(result).toEqual({
+        "page": 1,
+        "per_page": 1,
+        "photos": [],
+        "total_results": 1000,
+        "next_page": "https://api.pexels.com/v1/search/?page=2&per_page=1&query=tomatoes"
+        })
+      expect(fetch).toBeCalledWith(expect.stringMatching('tomatoes'))
+      expect(fetch).toHaveBeenCalledTimes(1)
+    })
+  })
