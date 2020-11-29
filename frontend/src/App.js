dea4b54fe364a0eb03cb1196c929b631ea3a90ef
Jana Bremer <jana.bremer@posteo.de>
2020-11-29 19:25:03 +0100

    Add small changes after review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,12 @@ import ProductPage from './pages/ProductPage'
 export default function App() {
 
   return (
-      <App>
+      <>
         <Switch>
           <Route exact path="/">
             <ProductPage productId={1} /> 
           </Route>
         </Switch>
-      </App>
+      </>
   );
 }
