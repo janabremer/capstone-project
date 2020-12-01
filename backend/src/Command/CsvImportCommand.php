0fb7ec43a0b6e4a586a8a561956eaf1a51a0f3d9
Jana Bremer <jana.bremer@posteo.de>
2020-12-01 14:19:29 +0100

    Add empty command to bin/console

---
+++ backend/src/Command/CsvImportCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Class CsvImportCommand
+ * @package App\ConsoleCommand
+ */
+class CsvImportCommand extends Command
+{
+    /**
+     * Configure
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('csv:import')
+            ->setDescription('Imports the CSV data file');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->success('Command exited cleanly');
+    }
+}
