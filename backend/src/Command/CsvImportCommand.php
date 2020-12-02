35d337948f627c73ce3efc37a9d913f5153d4724
Jana Bremer <jana.bremer@posteo.de>
2020-12-02 17:27:36 +0100

    Clean up

--- backend/src/Command/CsvImportCommand.php
+++ backend/src/Command/CsvImportCommand.php
@@ -3,12 +3,10 @@
 namespace App\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
-use League\Csv\Reader;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use App\Entity\Product;
 use App\Service\CsvReader;
 
 /**
