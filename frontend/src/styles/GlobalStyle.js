0b91f84360b098db84510aeaf231c2e9b6665d97
Jana Bremer <jana.bremer@posteo.de>
2020-12-16 18:16:11 +0100

    Test

--- frontend/src/styles/GlobalStyle.js
+++ frontend/src/styles/GlobalStyle.js
@@ -31,12 +31,11 @@ body {
     height: 100vh;
     letter-spacing: .03em;
     max-width: var(--max-width);
-    min-height: 100vh;
-    min-height: -webkit-fill-available;
-}
-
-html {
-  height: -webkit-fill-available;
+    
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
 }
 
 `
