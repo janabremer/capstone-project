ed73bbd2e27fb9c1c5c7d74e2491e9ff2c95c191
Jana Bremer <jana.bremer@posteo.de>
2020-12-17 08:53:20 +0100

    Add environment variable for api base url

--- frontend/src/services/getProductsPerPage.js
+++ frontend/src/services/getProductsPerPage.js
@@ -1,4 +1,6 @@
 export default function getProductsPerPage(pageLink) {
-    return fetch(`http://virtualwater.local${pageLink}`)
+    const apiBaseUrl = process.env.REACT_APP_API_BASE_URL
+    console.log(apiBaseUrl)
+    return fetch(`${apiBaseUrl}${pageLink}`)
       .then(res => res.json())
-  }
+}
