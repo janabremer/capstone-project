d30b549aab536e1023833e5b1d4293467ccd8d3a
Jana Bremer <jana.bremer@posteo.de>
2020-12-17 18:16:41 +0100

    Add small change on landing page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -17,7 +17,7 @@ export default function LandingPage() {
             <Content>
                 <p>We drink about 3 liters of water every day. And we need about 120 liters for washing, brushing our teeth and cooking. But we actually consume a lot more - around 4,000 liters per day!</p>
                 <p>Where does this water come from?</p>
-                <p>For the production of our food and all other products a lot of water is needed. This water is called "virtual water" or "hidden water".</p>
+                <p>For the production and the trade of our food a lot of water is needed. For example, for watering plants or for feeding animals. This water is called "virtual water" or "hidden water".</p>
                 <p>Discover the app and find out about the water in your food!</p>
             </Content>
             <PhotoLink />
